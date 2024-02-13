000cfb791c62e097342de98bc8da44b54ca03659
Arianna Reischer <tuo56767@temple.ed>
2024-02-13 02:08:57 -0500

    added all the files from the og receiptify

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
  */
 
 const client_id = '792207d6524f4255a1730e478d8b66f6';
-const redirect_uri = 'http://localhost:3000';
+const redirect_uri = 'http://ajreisc.github.io';
 const client_secret = 'fd5c90696d984ca7a65a54853f340c70';
